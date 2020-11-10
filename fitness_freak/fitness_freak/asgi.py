fdf810065ccede44a9ef98ade36cc4e1cdfd8524
Hammad Maqdoom <hammadmaqdoom1@gmail.com>
2020-11-11 03:02:31 +0500

    fix bugs

--- fitness_freak/fitness_freak/asgi.py
+++ fitness_freak/fitness_freak/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ff project.
+ASGI config for fitness_freak project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ff.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitness_freak.settings')
 
 application = get_asgi_application()
--- fitness_freak/fitness_freak/urls.py
+++ fitness_freak/fitness_freak/urls.py
@@ -1,4 +1,4 @@
-"""ff URL Configuration
+"""fitness_freak URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
